1cb43d400f43c5598cbe1d4a23021490125148cd
justin-guan <dengitzjg@gmail.com>
2017-06-28 17:20:19 -0400

    Added HotkeyBinder window

---
+++ src/main/java/app/discordkeys/controller/DiscordKeysHotkeyBinder.java
@@ -0,0 +1,7 @@
+package app.discordkeys.controller;
+
+/**
+ * Created by justin on 6/28/17.
+ */
+public class DiscordKeysHotkeyBinder {
+}
--- src/main/java/app/discordkeys/controller/DiscordKeysLoginController.java
+++ src/main/java/app/discordkeys/controller/DiscordKeysLoginController.java
@@ -4,29 +4,31 @@ import app.discordkeys.GlobalKeyListener;
 import app.discordkeys.JDAInstance;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 
 import javax.security.auth.login.LoginException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
  * Created by justin on 6/22/17.
  */
-public class DiscordKeysController implements Initializable {
+public class DiscordKeysLoginController implements Initializable {
 
     @FXML
     private TextField token;
-
-    @FXML
-    private Button login;
 
     @FXML
     private Label status;
@@ -38,19 +40,27 @@ public class DiscordKeysController implements Initializable {
 
     public void login(ActionEvent actionEvent) {
         try {
-            token.setDisable(true);
-            login.setDisable(true);
             JDAInstance.login(token.getText());
             status.setText("Connected to Discord");
             status.setTextFill(Color.GREEN);
             GlobalScreen.registerNativeHook();
             GlobalScreen.addNativeKeyListener(new GlobalKeyListener());
-        } catch (LoginException | InterruptedException | RateLimitedException e) {
-            System.out.println("Failed to login");
-            token.setDisable(false);
-            login.setDisable(false);
+
+            Parent root = FXMLLoader.load(getClass().getResource("/app/discordkeys/view/DiscordKeysHotkeyBinder.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("DiscordKeys Hotkey Binder");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (LoginException e) {
+            status.setText("Failed to Login: Login Failure");
+        } catch (InterruptedException e) {
+            status.setText("Failed to Login: Login Interrupted");
+        } catch (RateLimitedException e) {
+            status.setText("Failed to Login: Rate Limited");
         } catch (NativeHookException e) {
-            System.out.println("Failed to register native hook");
+            status.setText("Failed to register native hook");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
