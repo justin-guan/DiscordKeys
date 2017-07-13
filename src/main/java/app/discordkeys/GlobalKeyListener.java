e60ac735d47802cb038958cd7a29022c3c282cc1
justin-guan <dengitzjg@gmail.com>
2017-07-12 20:49:21 -0400

    Added send message to Discord functionality

--- src/main/java/app/discordkeys/GlobalKeyListener.java
+++ src/main/java/app/discordkeys/GlobalKeyListener.java
@@ -1,17 +1,11 @@
 package app.discordkeys;
 
 import javafx.application.Platform;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCombination;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.TextChannel;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 
-
 import java.util.HashSet;
-import java.util.List;
 
 /**
  * Created by justin on 6/23/17.
@@ -40,30 +34,11 @@ public class GlobalKeyListener implements NativeKeyListener {
             for (Shortcut s : shortcuts) {
                 System.out.println("Shortcuts: " + s.getShortcutAsNativeKeyEventList());
                 if (pressedKeys.containsAll(s.getShortcutAsNativeKeyEventList())) {
-                    System.out.println(s.getCommand());
+                    sendDiscordMessage(s.getChannel(), s.getCommand());
                 }
             }
             pressedKeys.remove(nativeKeyEvent.getKeyCode());
         });
-//        boolean isCtrlPressed = (nativeKeyEvent.getModifiers() & NativeKeyEvent.CTRL_MASK) != 0;
-//
-//        if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_F1 && isCtrlPressed) {
-//            sendDiscordMessage("!waow");
-//        } else if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_F2 && isCtrlPressed) {
-//            sendDiscordMessage("!ld");
-//        } else if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_F3 && isCtrlPressed) {
-//            sendDiscordMessage("!tucker");
-//        } else if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_F4 && isCtrlPressed) {
-//            sendDiscordMessage("!noone");
-//        } else if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_F5 && isCtrlPressed) {
-//            sendDiscordMessage("!cyka");
-//        } else if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_F6 && isCtrlPressed) {
-//            sendDiscordMessage("!free");
-//        } else if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_F7 && isCtrlPressed) {
-//            sendDiscordMessage("!price");
-//        } else if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_F8 && isCtrlPressed) {
-//            sendDiscordMessage("!maple");
-//        }
     }
 
     @Override
@@ -71,17 +46,8 @@ public class GlobalKeyListener implements NativeKeyListener {
         //System.out.println("Key Typed: " + nativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()));
     }
 
-    private void sendDiscordMessage(String msg) {
-        JDA jda = JDAInstance.getJda();
-        for (Guild g : jda.getGuilds()) {
-            if (g.toString().equals("G:Niggaz In The House(178372543986663425)")) {
-                for (TextChannel t : g.getTextChannels()) {
-                    if (t.toString().equals("TC:general(178372543986663425)")) {
-                        t.sendMessage(msg).queue();
-                    }
-                }
-            }
-        }
+    private void sendDiscordMessage(TextChannel t, String msg) {
+        t.sendMessage(msg).queue();
     }
 
     public void addNewShortcut(Shortcut shortcut) {
